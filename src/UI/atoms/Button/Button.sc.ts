ac005a3c0bba8c2b52594d784682d5d65d1cef7d
Monika Swidzinska <m.swidzinska@cheil.com>
2022-09-19 10:06:11 +0200

    button animation

--- src/UI/atoms/Button/Button.sc.ts
+++ src/UI/atoms/Button/Button.sc.ts
@@ -6,7 +6,7 @@ const { white, mediumRed, darkRed, grey, darkGrey } = colors;
 type StyledButtonProps = {
   color: "red" | "grey";
 };
-export const StyledButton = styled.a<StyledButtonProps>`
+export const StyledButton = styled.button<StyledButtonProps>`
   z-index: 1;
   padding: 0.5rem 2.5rem;
   transition: all 0.2s ease-in;
@@ -18,15 +18,22 @@ export const StyledButton = styled.a<StyledButtonProps>`
   text-decoration: none;
   width: fit-content;
   margin-right: 4rem;
-
+  border: none;
   ${({ color }) =>
     color === "red" &&
     css`
       color: ${white};
       background: ${darkRed};
+
       &:hover {
         background: ${mediumRed};
         color: ${white};
+        transform: translateY(-3px);
+        box-shadow: 0 10px 20px rgba(255, 255, 255, 0.1);
+      }
+      &:active {
+        transform: translateY(-2px);
+        box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
       }
     `}
   ${({ color }) =>
@@ -37,6 +44,12 @@ export const StyledButton = styled.a<StyledButtonProps>`
       &:hover {
         color: ${white};
         background-color: ${darkGrey};
+        transform: translateY(-3px);
+        box-shadow: 0 10px 20px rgba(255, 255, 255, 0.1);
       }
-    `}
+      &:active {
+        transform: translateY(-1px);
+        box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+      }
+    `};
 `;
